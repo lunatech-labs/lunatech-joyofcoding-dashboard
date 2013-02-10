9e6cefa60475744cc8d70f9a080112eee027b54a
Nicolas Leroux <leroux.nicolas@gmail.com>
2013-02-10 15:32:52 +0100

    initial release

---
+++ app/controllers/Application.scala
@@ -0,0 +1,35 @@
+package controllers
+
+
+import play.api._
+import play.api.mvc._
+
+import play.api.libs.json._
+import play.api.libs.iteratee._
+
+import play.api.libs.concurrent.Execution.Implicits._
+
+import models._
+
+
+object Application extends Controller {
+  
+  def index = Action {  implicit request =>
+    Ok(views.html.index("", Office.checkedIn, Office.motdMessage))
+  }
+  
+  def checkin(email:String) = Action {
+     Office.join(email)
+     Ok("")
+   }
+   
+  def checkout(email:String) = Action {
+     Office.quit(email)
+     Ok("")
+  }
+
+  def listen() = WebSocket.async[JsValue] { request  =>
+    Office.listen()
+  }
+  
+}
